10cafb8cfb3dfa58308194317afb2729a0a828fc
papande4 <pawanpandey031@gmail.com>
2018-06-26 12:37:07 +0530

    dfafd

---
+++ ansr/ansr.js
@@ -0,0 +1,16 @@
+function popup(){
+    y=document.getElementById("pop");
+    y.style.display="block";
+}
+
+function popout(){
+    y=document.getElementById("pop");
+    y.style.display="none";
+
+}
+
+function dikhao(){
+    a=document.getElementsById("head");
+    a.style.display="block";
+}
+
